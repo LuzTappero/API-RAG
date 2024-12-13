90a6c1fd782db9f2370d8b6552180e00355b7d81
Luz.Tappero <luztappero@gmail.com>
2024-12-13 12:38:04 -0300

    change response

--- services/generate_response_with_llm.py
+++ services/generate_response_with_llm.py
@@ -6,13 +6,13 @@ co = cohere.ClientV2(COHERE_API_KEY)
 
 system_prompt = """
         Tu trabajo es responder a las preguntas, con las siguientes características:
+        - Solo debes utilizar el contenido de las historias para responder sobre las preguntas del usuario.
+        - Si no tienes información sobre algun contenido, di 'Lo siento, no tengo información sobre eso.'
         - Responde de manera amigable y con tono entusiasta, como si le hablaras a un niño.
         - Responde en máximo 3 oraciones.
         - Agrega emojis a la respuesta.
         - Ante la misma pregunta debes responder lo más similar posible para cada interacción.
         - Responde siempre en español, sin importar en qué idioma se haga la pregunta.
-        - Solo debes utilizar el contenido de las historias para responder sobre las preguntas del usuario.
-        - Si no tienes información sobre algun contenido, di 'Lo siento, no tengo información sobre eso.'
         """
 
 def generate_response(most_relevant_content, question_text):
@@ -20,13 +20,13 @@ def generate_response(most_relevant_content, question_text):
     try:
         prompt_con_instrucciones = f"{system_prompt}\nRelevant text: {most_relevant_content}\n\Question: {question_text}"
 
-        response = co.generate(
+        response = co.chat(
             model="command-r-plus-08-2024",
-            prompt=prompt_con_instrucciones,
+            messages=[{"role": "user", "content": prompt_con_instrucciones}],
             max_tokens=100,
             temperature=0.2,
         )
-        return response.generations[0].text.strip()
+        return response.message.content[0].text
     except Exception as e:
             print(f"Error generating response with Cohere: {e}")
             return "There was a problem generating the response. Please try again."
